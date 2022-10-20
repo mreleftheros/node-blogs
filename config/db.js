fd0866bd208b8f45eb73236017db7d9bf3f88d47
eleftheros <eleftheros_all@protonmail.com>
2022-10-20 17:23:47 +0300

    Change autocomplete in form

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 
 const dev = process.env.NODE_ENV === "dev";
 
-const _dbName = dev ? "test" : "projects";
+const _dbName = dev ? "projects" : "projects";
 const _colName = "blogs";
 const _client = new MongoClient(dev ? process.env.DB_URI_DEV : process.env.DB_URI);
 
